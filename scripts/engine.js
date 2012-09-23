5d2acbd07b864c874534f0723d2b9b6835af3c70
Niels Leenheer <niels@leenheer.nl>
2012-09-23 20:43:33 +0200

    Fix a typo in the requestFullscreen test

--- scripts/engine.js
+++ scripts/engine.js
@@ -2883,8 +2883,8 @@ Test = (function() {
 			
 			this.section.setItem({
 				id:			'requestFullScreen',
-				passed:		!! document.documentElement.requestFullScreen || !! document.documentElement.webkitRequestFullScreen || !! document.documentElement.mozRequestFullScreen || !! document.documentElement.msRequestFullScreen || !! document.documentElement.oRequestFullScreen, 
 				value: 		2
+				passed:		!! document.documentElement.requestFullscreen || !! document.documentElement.webkitRequestFullScreen || !! document.documentElement.mozRequestFullScreen || !! document.documentElement.msRequestFullScreen, 
 			});
 			
 			this.section.setItem({
