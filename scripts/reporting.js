8dfab1b5a66960e45bcdc8096a417c40457dea31
Niels Leenheer <niels@leenheer.nl>
2012-10-05 08:55:11 +0200

    Prevent an error in IE6

--- scripts/reporting.js
+++ scripts/reporting.js
@@ -12,6 +12,7 @@
 			});
 		}
 		function addListen(obj, i){
+			if(!obj) return obj;
 			if(i = obj.length)while(i--)obj[i].addEventListener = addEvent;
 			else obj.addEventListener = addEvent;
 			return obj;
