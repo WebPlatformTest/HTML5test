92e59f9dc57ee772676ca72d8191957684403999
Niels Leenheer <niels@leenheer.nl>
2016-06-29 09:38:55 +0200

    Make the template script compatible with PHP 7

--- backend/libraries/template.php
+++ backend/libraries/template.php
@@ -1,6 +1,7 @@
 <?php
 
 class Template {
+	var $file;
 	var $filters;
     var $value;
 	var $prepend;
@@ -11,7 +12,7 @@ class Template {
      *
      * @param $file string the file name you want to load
      */
-    function Template($file = null) {
+    function __construct($file = null) {
         $this->file = $file;
     }
 
@@ -35,7 +36,7 @@ class Template {
 		else
 			$this->prepend[$name] = is_object($value) && @function_exists(array($value, 'Template')) ? $value->fetch() : $value;
     }
-	
+
 	function filter($function) {
 		$this->filters[] = $function;
 	}
@@ -50,18 +51,16 @@ class Template {
      * @param $file string the template file name
      */
     function fetch($file = null) {
-		global $manager, $user, $tree, $lang, $config, $site;
-		
         if(!$file) $file = $this->file;
-		
+
 		if (is_array($this->prepend)) {
 			while(list($k,) = each($this->prepend)) {
 				if (isset($this->value[$k]))
 					$this->value[$k] = $this->prepend[$k] . $this->value[$k];
 				else
 					$this->value[$k] = $this->prepend[$k];
-			} 
-		}     
+			}
+		}
 
 		if (is_array($this->append)) {
 			while(list($k,) = each($this->append)) {
@@ -69,26 +68,26 @@ class Template {
 					$this->value[$k] = $this->value[$k] . $this->append[$k];
 				else
 					$this->value[$k] = $this->append[$k];
-			} 
-		}     
+			}
+		}
 
 		if (is_array($this->value)) {
-         	extract($this->value);     
-		}     
+         	extract($this->value);
+		}
 									   // Extract the vars to local namespace
-        
+
 		ob_start();                    // Start output buffering
         include($file);                // Include the file
         $contents = ob_get_contents(); // Get the contents of the buffer
         ob_end_clean();                // End buffering and discard
-        
+
 		// Execute any registered filters...
 		if (is_array($this->filters)) {
 			while(list(,$f) = each($this->filters)) {
 				$contents = call_user_func($f, $contents, $this);
 			}
 		}
-		
+
 		return $contents;              // Return the contents
     }
 }
