df1a35367e3de6c87c5e52812980a6656ed8d639
Niels Leenheer <niels@creatype.nl>
2017-12-04 21:18:51 +0100

    Reorganize some scripts to make the logic more clear

--- scripts/base.js
+++ scripts/base.js
@@ -108,7 +108,114 @@
 		load();
 	}
 
+	function submit(method, payload) {
+		var httpRequest;
+		if (window.XMLHttpRequest) {
+			httpRequest = new XMLHttpRequest();
+		} else if (window.ActiveXObject) {
+			httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+		}
 
+		httpRequest.open('POST','/api/' + method, true);
+		httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+		httpRequest.send('payload=' + encodeURIComponent(payload));
+	}
+
+	function decodeParameters() {
+		var params = {};
+		
+		if (location.search) {
+			var parts = location.search.substring(1).split('&');
+
+			for (var i = 0; i < parts.length; i++) {
+				var nv = parts[i].split('=');
+				if (!nv[0]) continue;
+				params[nv[0]] = decodeURIComponent(nv[1]) || true;
+			}
+		}
+
+		return params;
+	}
+
+	function upgradeConnection(success, failure) {
+		if (location.protocol == "http:") {
+			var httpRequest;
+
+			if (window.XMLHttpRequest) {
+				httpRequest = new XMLHttpRequest();
+			} else if (window.ActiveXObject) {
+				httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+			}
+			
+			httpRequest.onreadystatechange = function() {
+				if (httpRequest.readyState == 4) {
+					if (httpRequest.status >= 200 && httpRequest.status < 400) {
+						success();
+					} else {
+						failure();
+					}
+				}
+			}
+
+			httpRequest.open('GET','https://' + location.hostname + '/assets/upgrade', true);
+			httpRequest.send();
+
+			return;
+		}
+		
+		failure();
+	}
+
+	function submitResults(r, c) {
+		var parameters = decodeParameters();
+		var identifier = typeof parameters.identifier != 'undefined' ? parameters.identifier : '';
+		var source = typeof parameters.source != 'undefined' ? parameters.source : '';
+		var task = typeof parameters.task != 'undefined' ? parameters.task : '';
+
+		try {
+			var payload = '{' +
+						'"release": "' + r.release + '",' +
+						'"source": "' + source + '",' +
+						'"protocol": "' + location.protocol + '",' +
+						'"identifier": "' + escapeSlashes(identifier) + '",' +
+						'"task": "' + task + '",' +
+						'"uniqueid": "' + r.uniqueid + '",' +
+						'"score": ' + c.score + ',' +
+						'"maximum": ' + c.maximum + ',' +
+						'"camouflage": "' + (Browsers.camouflage ? '1' : '0') + '",' +
+						'"features": "' + (Browsers.features.join(',')) + '",' +
+						'"browserName": "' + (Browsers.browser.name ? Browsers.browser.name : '') + '",' +
+						'"browserChannel": "' + (Browsers.browser.channel ? Browsers.browser.channel : '') + '",' +
+						'"browserVersion": "' + (Browsers.browser.version ? Browsers.browser.version.toString() : '') + '",' +
+						'"browserVersionType": "' + (Browsers.browser.version ? Browsers.browser.version.type : '') + '",' +
+						'"browserVersionMajor": "' + (Browsers.browser.version ? Browsers.browser.version.major : '') + '",' +
+						'"browserVersionMinor": "' + (Browsers.browser.version ? Browsers.browser.version.minor : '') + '",' +
+						'"browserVersionOriginal": "' + (Browsers.browser.version ? Browsers.browser.version.original : '') + '",' +
+						'"browserMode": "' + (Browsers.browser.mode ? Browsers.browser.mode : '') + '",' +
+						'"engineName": "' + (Browsers.engine.name ? Browsers.engine.name : '') + '",' +
+						'"engineVersion": "' + (Browsers.engine.version ? Browsers.engine.version.toString() : '') + '",' +
+						'"osName": "' + (Browsers.os.name ? Browsers.os.name : '') + '",' +
+						'"osFamily": "' + (Browsers.os.family ? Browsers.os.family : '') + '",' +
+						'"osVersion": "' + (Browsers.os.version ? Browsers.os.version.toString() : '') + '",' +
+						'"deviceManufacturer": "' + (Browsers.device.manufacturer ? Browsers.device.manufacturer : '') + '",' +
+						'"deviceModel": "' + (Browsers.device.model ? Browsers.device.model : '') + '",' +
+						'"deviceSeries": "' + (Browsers.device.series ? Browsers.device.series : '') + '",' +
+						'"deviceType": "' + (Browsers.device.type ? Browsers.device.type : '') + '",' +
+						'"deviceIdentified": "' + (Browsers.device.identified ? '1' : '0' ) + '",' +
+						'"deviceWidth": "' + (screen.width) + '",' +
+						'"deviceHeight": "' + (screen.height) + '",' +
+						'"useragent": "' + navigator.userAgent + '",' +
+						'"humanReadable": "' + Browsers.toString() + '",' +
+						'"points": "' + c.points + '",' +
+						'"results": "' + r.results + '"' +
+						'}';
+
+			submit('submit', payload);
+		} catch(e) {
+			alert('Could not submit results: ' + e.message);
+		}
+	}
+	
 	var tim = (function(){
 	    var starts  = "\\{\\{",
 	        ends    = "\\}\\}",
